1b5e1513274da6d72040868eb20c163c581729c3
root <root@sfdxjayree.us-central1-a.c.silicon-glyph-307007.internal>
2021-05-05 10:09:52 +0000

    DevOps assignment

--- web-server/runner.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app')
-
-app.start(() => console.log('started app...'))
-
-const stop = () => app.stop(() => console.log('stopped app...'))
-
-process.once('SIGINT', stop)
-process.once('SIGTERM', stop)
